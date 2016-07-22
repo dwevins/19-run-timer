2f7c79df01549bd41f117c0e2b1f44199a3947b4
Danny Evins <dcastkid@gmail.com>
2016-07-22 11:42:36 -0500

    add index model(), index link-to new-run

--- app/review/route.js
+++ app/review/route.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model ({ id }) {
+    return fetch(`//http://tiny-tn.herokuapp.com/collections/runs-dwe/${id}`)
+      .then((res) => rs.json());
 });
--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('review');
+  this.route('review', { path: 'review/id/:id' });
   this.route('new-run');
 });
 
