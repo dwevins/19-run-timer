c310278a7a49d7ffd3c7719d725197a0c5ab61b4
Danny Evins <dcastkid@gmail.com>
2016-07-23 09:36:07 -0500

    add edit-run stuff

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('review', { path: 'review/id/:id' });
   this.route('new-run');
+  this.route('edit-run');
 });
 
 export default Router;
